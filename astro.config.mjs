ea1d46aa53dcc276a69609564c86dc7ade06a3a5
Rio Bahtiar <rio.bahtiar@sekolahmu.co.id>
2022-10-16 23:53:07 +0700

    add tailwind, daisyUi and deno fmt checker

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 import deno from "@astrojs/deno";
@@ -9,9 +9,15 @@ import deno from "@astrojs/deno";
 import react from "@astrojs/react";
 
 // https://astro.build/config
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
+import prefetch from "@astrojs/prefetch";
+
+// https://astro.build/config
 export default defineConfig({
-  site: 'https://rio.deno.dev',
-  integrations: [mdx(), sitemap(), react()],
+  site: "https://rio.deno.dev",
+  integrations: [mdx(), sitemap(), react(), tailwind(), prefetch()],
   output: "server",
-  adapter: deno()
+  adapter: deno(),
 });
--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Rio Bahtiar | Blog';
-export const SITE_DESCRIPTION = 'Hi All, Welcome to Rio Bahtiar Blog';
+export const SITE_TITLE = "Rio Bahtiar | Blog";
+export const SITE_DESCRIPTION = "Hi All, Welcome to Rio Bahtiar Blog";
