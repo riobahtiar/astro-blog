22951d07f198df30cf379a9fd2f5402c050da244
Rio Bahtiar <rio.bahtiar@sekolahmu.co.id>
2022-10-17 02:06:40 +0700

    minor update menu

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,5 +4,11 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [require("daisyui")],
+  plugins: [
+    require("@tailwindcss/aspect-ratio"),
+    require("@tailwindcss/line-clamp"),
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/forms"),
+    require("daisyui"),
+  ],
 };
