438f9295cb54a71c292ba28f926abf0366938d4a
Rio Bahtiar <rio.bahtiar@sekolahmu.co.id>
2022-10-16 21:16:00 +0700

    test deno deploy 2

--- src/config.ts
+++ src/config.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Rio Bahtiar | Blog';
-export const SITE_DESCRIPTION = 'Welcome to Rio Bahtiar Blog';
+export const SITE_DESCRIPTION = 'Hi All, Welcome to Rio Bahtiar Blog';
